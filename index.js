91a83f6cbc19ec712ae6979d4ba321c4789edab3
petreOOP <40559295+petreOOP@users.noreply.github.com>
2020-08-03 18:23:35 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function selecting() {
+
+    const div = document.querySelector('div');
+    const linkButton = div.querySelector('.link');
+
+    linkButton.addEventListener('click', function() {
+        alert ('Thanks for visiting our website');
+    });
+
+}
+
+selecting();
